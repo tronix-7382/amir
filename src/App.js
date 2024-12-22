5ed7a9f4eeb3ecd1aa543fca44aff59b7b6577a3
tronix <rnoorh.work@gmail.com>
2024-12-22 16:05:38 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Logo from "./result.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Logo} className="App-logo" alt="logo" />
+        <p>Hello , Im a Retard.</p>
+        <a className="App-link">Better User interface Soon...</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
