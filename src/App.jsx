e2b908dfd729777614cb40cdefd8313d2036b4f2
tronix <rnoorh.work@gmail.com>
2024-12-22 16:39:49 +0330

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import Logo from "./result.png";
+import Audi from './audio.mp3'
 import "./App.css";
+import { useRef } from "react";
 
 function App() {
+  const ref = useRef()
+  
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={Logo} className="App-logo" alt="logo" />
+        <img src={Logo} className="App-logo" alt="logo" onClick={()=> ref.current.play()} />
         <p>Hello , Im a Retard.</p>
         <a className="App-link">Better User interface Soon...</a>
       </header>
-    </div>
+      <audio ref={ref} src={Audi} ></audio>
+    </div>  
   );
 }
 
